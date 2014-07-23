a5ff2678d3c883d51946d2b43707528c6e3d868f
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-07-23 23:28:27 +0200

    Initial commit: AreaOrderComparator.java

---
+++ ohmdb-core/src/main/java/com/ohmdb/filestore/AreaOrderComparator.java
@@ -0,0 +1,32 @@
+package com.ohmdb.filestore;
+
+/*
+ * #%L
+ * ohmdb-core
+ * %%
+ * Copyright (C) 2013 - 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.util.Comparator;
+
+public class AreaOrderComparator implements Comparator<Area> {
+
+	@Override
+	public int compare(Area a1, Area a2) {
+		return (int) Math.signum(a1.order - a2.order);
+	}
+
+}
