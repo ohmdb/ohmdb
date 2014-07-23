6d7248469e927158742adc70e573753eeda38091
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-07-23 23:28:29 +0200

    Initial commit: ProxyUtil.java

---
+++ ohmdb-core/src/main/java/com/ohmdb/util/ProxyUtil.java
@@ -0,0 +1,37 @@
+package com.ohmdb.util;
+
+/*
+ * #%L
+ * ohmdb-core
+ * %%
+ * Copyright (C) 2013 - 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+public class ProxyUtil {
+
+	@SuppressWarnings("unchecked")
+	public static <T> T createProxy(InvocationHandler handler, Class<?>... interfaces) {
+		return ((T) Proxy.newProxyInstance(ProxyUtil.class.getClassLoader(), interfaces, handler));
+	}
+
+	public static <T> T tracer(Class<T> interfacee, T implementation) {
+		return createProxy(new TraceHandler(implementation), interfacee);
+	}
+
+}
