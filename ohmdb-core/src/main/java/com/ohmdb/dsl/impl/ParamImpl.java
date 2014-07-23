0e2fe25ea31b2b437c7e7f5ecf14f8fc8fd74cbf
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-07-23 23:28:28 +0200

    Initial commit: ParamImpl.java

---
+++ ohmdb-core/src/main/java/com/ohmdb/dsl/impl/ParamImpl.java
@@ -0,0 +1,78 @@
+package com.ohmdb.dsl.impl;
+
+/*
+ * #%L
+ * ohmdb-core
+ * %%
+ * Copyright (C) 2013 - 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.ohmdb.api.Parameter;
+import com.ohmdb.api.ParameterBinding;
+
+public class ParamImpl<T> implements Parameter<T> {
+
+	private final String name;
+	private final Class<T> type;
+
+	public ParamImpl(String name, Class<T> type) {
+		this.name = name;
+		this.type = type;
+	}
+
+	@Override
+	public String name() {
+		return name;
+	}
+
+	@Override
+	public ParameterBinding<T> as(T value) {
+		return new ParamBindingImpl<T>(this, value);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		@SuppressWarnings("rawtypes")
+		ParamImpl other = (ParamImpl) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+}
