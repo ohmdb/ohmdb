47118134734c0acfc7fabdd44a939fd3f26b7f4e
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-07-23 23:28:29 +0200

    Initial commit: U.java

---
+++ ohmdb-core/src/main/java/com/ohmdb/util/U.java
@@ -0,0 +1,673 @@
+package com.ohmdb.util;
+
+/*
+ * #%L
+ * ohmdb-core
+ * %%
+ * Copyright (C) 2013 - 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class U {
+
+	private static final Map<String, TypeKind> KINDS = initKinds();
+
+	private static Map<String, TypeKind> initKinds() {
+
+		Map<String, TypeKind> kinds = new HashMap<String, TypeKind>();
+
+		kinds.put("boolean", TypeKind.BOOLEAN);
+
+		kinds.put("byte", TypeKind.BYTE);
+
+		kinds.put("char", TypeKind.CHAR);
+
+		kinds.put("short", TypeKind.SHORT);
+
+		kinds.put("int", TypeKind.INT);
+
+		kinds.put("long", TypeKind.LONG);
+
+		kinds.put("float", TypeKind.FLOAT);
+
+		kinds.put("double", TypeKind.DOUBLE);
+
+		kinds.put("java.lang.String", TypeKind.STRING);
+
+		kinds.put("java.lang.Boolean", TypeKind.BOOLEAN_OBJ);
+
+		kinds.put("java.lang.Byte", TypeKind.BYTE_OBJ);
+
+		kinds.put("java.lang.Character", TypeKind.CHAR_OBJ);
+
+		kinds.put("java.lang.Short", TypeKind.SHORT_OBJ);
+
+		kinds.put("java.lang.Integer", TypeKind.INT_OBJ);
+
+		kinds.put("java.lang.Long", TypeKind.LONG_OBJ);
+
+		kinds.put("java.lang.Float", TypeKind.FLOAT_OBJ);
+
+		kinds.put("java.lang.Double", TypeKind.DOUBLE_OBJ);
+
+		kinds.put("java.util.Date", TypeKind.DATE);
+
+		return kinds;
+	}
+
+	/**
+	 * @return Any kind, except NULL
+	 */
+	public static TypeKind kindOf(Class<?> type) {
+		String typeName = type.getName();
+		TypeKind kind = U.KINDS.get(typeName);
+
+		if (kind == null) {
+			kind = TypeKind.OBJECT;
+		}
+
+		return kind;
+	}
+
+	/**
+	 * @return Any kind, including NULL
+	 */
+	public static TypeKind kindOf(Object value) {
+		if (value == null) {
+			return TypeKind.NULL;
+		}
+
+		String typeName = value.getClass().getName();
+		TypeKind kind = U.KINDS.get(typeName);
+
+		if (kind == null) {
+			kind = TypeKind.OBJECT;
+		}
+
+		return kind;
+	}
+
+	public static void sleep(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+		}
+	}
+
+	public static boolean waitInterruption(long millis) {
+		try {
+			Thread.sleep(millis);
+			return true;
+		} catch (InterruptedException e) {
+			Thread.interrupted();
+			return false;
+		}
+	}
+
+	public static String text(Collection<Object> coll) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+
+		boolean first = true;
+
+		for (Object obj : coll) {
+			if (!first) {
+				sb.append(", ");
+			}
+
+			sb.append(text(obj));
+			first = false;
+		}
+
+		sb.append("]");
+		return sb.toString();
+	}
+
+	public static String text(Object obj) {
+		if (obj == null) {
+			return "null";
+		} else if (obj instanceof byte[]) {
+			return Arrays.toString((byte[]) obj);
+		} else if (obj instanceof short[]) {
+			return Arrays.toString((short[]) obj);
+		} else if (obj instanceof int[]) {
+			return Arrays.toString((int[]) obj);
+		} else if (obj instanceof long[]) {
+			return Arrays.toString((long[]) obj);
+		} else if (obj instanceof float[]) {
+			return Arrays.toString((float[]) obj);
+		} else if (obj instanceof double[]) {
+			return Arrays.toString((double[]) obj);
+		} else if (obj instanceof boolean[]) {
+			return Arrays.toString((boolean[]) obj);
+		} else if (obj instanceof Object[]) {
+			return text((Object[]) obj);
+		} else {
+			return String.valueOf(obj);
+		}
+	}
+
+	public static String text(Object[] objs) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+
+		for (int i = 0; i < objs.length; i++) {
+			if (i > 0) {
+				sb.append(", ");
+			}
+			sb.append(text(objs[i]));
+		}
+
+		sb.append("]");
+
+		return sb.toString();
+	}
+
+	public static String text(Iterator<?> it) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+
+		boolean first = true;
+
+		while (it.hasNext()) {
+			if (first) {
+				sb.append(", ");
+				first = false;
+			}
+
+			sb.append(text(it.next()));
+		}
+
+		sb.append("]");
+
+		return sb.toString();
+	}
+
+	public static String textln(Object[] objs) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+
+		for (int i = 0; i < objs.length; i++) {
+			if (i > 0) {
+				sb.append(",");
+			}
+			sb.append("\n  ");
+			sb.append(text(objs[i]));
+		}
+
+		sb.append("\n]");
+
+		return sb.toString();
+	}
+
+	public static String replaceText(String s, String[][] repls) {
+		for (String[] repl : repls) {
+			s = s.replaceAll(Pattern.quote(repl[0]), repl[1]);
+		}
+		return s;
+	}
+
+	public static String join(Object[] objects, String sep) {
+		return render(objects, "%s", sep);
+	}
+
+	public static String render(Object[] objects, String format, String sep) {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < objects.length; i++) {
+			if (i > 0) {
+				sb.append(sep);
+			}
+			sb.append(String.format(format, objects[i]));
+		}
+
+		return sb.toString();
+	}
+
+	public static InputStream resource(String filename) {
+		return U.class.getClassLoader().getResourceAsStream(filename);
+	}
+
+	public static boolean isDevEnv() {
+		return true;
+	}
+
+	public static void time() {
+		System.out.println(new Date());
+	}
+
+	public static boolean xor(boolean a, boolean b) {
+		return a && !b || b && !a;
+	}
+
+	public static boolean eq(Object a, Object b) {
+		if (a == null && b == null) {
+			return true;
+		}
+
+		if (a == null) {
+			return false;
+		}
+
+		return a.equals(b);
+	}
+
+	public static void failIf(boolean cond, String msg) {
+		if (cond) {
+			throw new RuntimeException(msg);
+		}
+	}
+
+	public static String load(String name) {
+
+		InputStream stream = U.class.getClassLoader().getResourceAsStream(name);
+
+		InputStreamReader reader = new InputStreamReader(stream);
+
+		BufferedReader r = new BufferedReader(reader);
+
+		StringBuilder sb = new StringBuilder();
+		String line;
+		try {
+			while ((line = r.readLine()) != null) {
+				sb.append(line);
+			}
+		} catch (IOException e) {
+			throw Errors.rte("Cannot read resource: " + name, e);
+		}
+
+		return sb.toString();
+	}
+
+	public static int indexOf(int[] array, int valueToFind, int startIndex) {
+		if (array == null) {
+			return -1;
+		}
+		if (startIndex < 0) {
+			startIndex = 0;
+		}
+		for (int i = startIndex; i < array.length; i++) {
+			if (valueToFind == array[i]) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static int indexOf(Object[] array, Object valueToFind, int startIndex) {
+		if (array == null) {
+			return -1;
+		}
+		if (startIndex < 0) {
+			startIndex = 0;
+		}
+		for (int i = startIndex; i < array.length; i++) {
+			if (valueToFind == array[i]) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static String replace(String s, String regex, Replacer replacer) {
+		StringBuffer output = new StringBuffer();
+		Pattern p = Pattern.compile(regex);
+		Matcher matcher = p.matcher(s);
+
+		while (matcher.find()) {
+			int len = matcher.groupCount() + 1;
+			String[] gr = new String[len];
+
+			for (int i = 0; i < gr.length; i++) {
+				gr[i] = matcher.group(i);
+			}
+
+			String rep = replacer.replace(gr);
+			matcher.appendReplacement(output, rep);
+		}
+
+		matcher.appendTail(output);
+		return output.toString();
+	}
+
+	public static void delete(String filename) {
+		new File(filename).delete();
+	}
+
+	public static <T> T[] expand(T[] arr, T item) {
+		int len = arr.length;
+
+		arr = Arrays.copyOf(arr, len + 1);
+		arr[len] = item;
+
+		return arr;
+	}
+
+	public static <T> T[] range(T[] arr, int from, int to) {
+		int start = from >= 0 ? from : arr.length + from;
+		int end = to >= 0 ? to : arr.length + to;
+
+		if (start < 0) {
+			start = 0;
+		}
+
+		if (end > arr.length - 1) {
+			end = arr.length - 1;
+		}
+
+		Check.arg(start <= end, "Invalid range: expected form <= to!");
+
+		int size = end - start + 1;
+
+		T[] part = Arrays.copyOf(arr, size);
+
+		System.arraycopy(arr, start, part, 0, size);
+
+		return part;
+	}
+
+	public static <T> Set<T> set(T... values) {
+		Set<T> set = new HashSet<T>();
+
+		for (T val : values) {
+			set.add(val);
+		}
+
+		return set;
+	}
+
+	public static void waitFor(AtomicBoolean done) {
+		while (!done.get()) {
+			U.sleep(10);
+		}
+	}
+
+	public static void waitFor(AtomicInteger n, int value) {
+		while (n.get() != value) {
+			U.sleep(10);
+		}
+	}
+
+	public static boolean lt(Comparable<Object> a, Comparable<Object> b) {
+		return a.compareTo(b) < 0;
+	}
+
+	public static boolean lte(Comparable<Object> a, Comparable<Object> b) {
+		return a.compareTo(b) <= 0;
+	}
+
+	public static boolean gt(Comparable<Object> a, Comparable<Object> b) {
+		return a.compareTo(b) > 0;
+	}
+
+	public static boolean gte(Comparable<Object> a, Comparable<Object> b) {
+		return a.compareTo(b) >= 0;
+	}
+
+	public static boolean eq(Comparable<Object> a, Comparable<Object> b) {
+		return a.compareTo(b) == 0;
+	}
+
+	public static boolean neq(Comparable<Object> a, Comparable<Object> b) {
+		return a.compareTo(b) != 0;
+	}
+
+	@SuppressWarnings("rawtypes")
+	public static long[] cmp2long(Comparable[] arr) {
+		long[] arr2 = new long[arr.length];
+
+		cmp2long(arr, arr2, arr.length);
+
+		return arr2;
+	}
+
+	@SuppressWarnings("rawtypes")
+	public static void cmp2long(Comparable[] arr, long[] arr2, int n) {
+		for (int i = 0; i < n; i++) {
+			arr2[i] = (Long) arr[i];
+		}
+	}
+
+	public static byte[] serialize(Object value) {
+		try {
+			ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+			ObjectOutputStream out = new ObjectOutputStream(output);
+			out.writeObject(value);
+			output.close();
+
+			return output.toByteArray();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static Object deserialize(byte[] buf) {
+		try {
+			ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(buf));
+			Object obj = in.readObject();
+			in.close();
+			return obj;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void serialize(Object value, ByteBuffer buf) {
+		byte[] bytes = serialize(value);
+		buf.putInt(bytes.length);
+		buf.put(bytes);
+	}
+
+	public static Object deserialize(ByteBuffer buf) {
+		int len = buf.getInt();
+		byte[] bytes = new byte[len];
+		buf.get(bytes);
+		return deserialize(bytes);
+	}
+
+	public static void encode(Object value, ByteBuffer buf) {
+		TypeKind kind = kindOf(value);
+		int ordinal = kind.ordinal();
+		assert ordinal < 128;
+
+		byte kindCode = (byte) ordinal;
+		buf.put(kindCode);
+
+		switch (kind) {
+
+		case NULL:
+			// nothing else needed
+			break;
+
+		case BOOLEAN:
+		case BYTE:
+		case SHORT:
+		case CHAR:
+		case INT:
+		case LONG:
+		case FLOAT:
+		case DOUBLE:
+			throw Errors.notExpected();
+
+		case STRING:
+			String str = (String) value;
+			byte[] bytes = str.getBytes();
+			// 0-255
+			int len = bytes.length;
+			if (len < 255) {
+				byte len2 = (byte) (len - 128);
+				buf.put(len2);
+			} else {
+				byte len2 = (byte) (255 - 128);
+				buf.put(len2);
+				buf.putInt(len);
+			}
+			buf.put(bytes);
+			break;
+
+		case BOOLEAN_OBJ:
+			boolean val = (Boolean) value;
+			buf.put((byte) (val ? 1 : 0));
+			break;
+
+		case BYTE_OBJ:
+			buf.put((Byte) value);
+			break;
+
+		case SHORT_OBJ:
+			buf.putShort((Short) value);
+			break;
+
+		case CHAR_OBJ:
+			buf.putChar((Character) value);
+			break;
+
+		case INT_OBJ:
+			buf.putInt((Integer) value);
+			break;
+
+		case LONG_OBJ:
+			buf.putLong((Long) value);
+			break;
+
+		case FLOAT_OBJ:
+			buf.putFloat((Float) value);
+			break;
+
+		case DOUBLE_OBJ:
+			buf.putDouble((Double) value);
+			break;
+
+		case OBJECT:
+			serialize(value, buf);
+			break;
+
+		case DATE:
+			buf.putLong(((Date) value).getTime());
+			break;
+
+		default:
+			throw Errors.notExpected();
+		}
+	}
+
+	public static Object decode(ByteBuffer buf) {
+		byte kindCode = buf.get();
+
+		TypeKind kind = TypeKind.values()[kindCode];
+
+		switch (kind) {
+
+		case NULL:
+			return null;
+
+		case BOOLEAN:
+		case BOOLEAN_OBJ:
+			return buf.get() != 0;
+
+		case BYTE:
+		case BYTE_OBJ:
+			return buf.get();
+
+		case SHORT:
+		case SHORT_OBJ:
+			return buf.getShort();
+
+		case CHAR:
+		case CHAR_OBJ:
+			return buf.getChar();
+
+		case INT:
+		case INT_OBJ:
+			return buf.getInt();
+
+		case LONG:
+		case LONG_OBJ:
+			return buf.getLong();
+
+		case FLOAT:
+		case FLOAT_OBJ:
+			return buf.getFloat();
+
+		case DOUBLE:
+		case DOUBLE_OBJ:
+			return buf.getDouble();
+
+		case STRING:
+			byte len = buf.get();
+			int realLen = len + 128;
+			if (realLen == 255) {
+				realLen = buf.getInt();
+			}
+			byte[] sbuf = new byte[realLen];
+			buf.get(sbuf);
+			return new String(sbuf);
+
+		case OBJECT:
+			return deserialize(buf);
+
+		case DATE:
+			return new Date(buf.getLong());
+
+		default:
+			throw Errors.notExpected();
+		}
+	}
+
+	public static byte[] buf2bytes(ByteBuffer buf) {
+		ByteBuffer buf2 = buf.duplicate();
+
+		buf2.rewind();
+		buf2.limit(buf2.capacity());
+
+		byte[] bytes = new byte[buf2.capacity()];
+		buf2.get(bytes);
+
+		return bytes;
+	}
+
+	public static ByteBuffer buf(byte[] bytes) {
+		ByteBuffer buf = ByteBuffer.allocate(bytes.length);
+		buf.put(bytes);
+		buf.rewind();
+
+		return buf;
+	}
+
+}
