18a8b6bdb59ecad01e0713048a6ff47aee2a62c4
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-07-23 23:28:28 +0200

    Initial commit: ManyToOneImpl.java

---
+++ ohmdb-core/src/main/java/com/ohmdb/dsl/rel/impl/ManyToOneImpl.java
@@ -0,0 +1,58 @@
+package com.ohmdb.dsl.rel.impl;
+
+/*
+ * #%L
+ * ohmdb-core
+ * %%
+ * Copyright (C) 2013 - 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.ohmdb.abstracts.RWRelation;
+import com.ohmdb.api.ManyToOne;
+import com.ohmdb.api.OneToMany;
+
+public class ManyToOneImpl<FROM, TO> extends AbstractCommonRelation<FROM, TO> implements ManyToOne<FROM, TO> {
+
+	public ManyToOneImpl(RWRelation relation) {
+		super(relation);
+	}
+
+	@Override
+	public OneToMany<TO, FROM> inversed() {
+		return new OneToManyImpl<TO, FROM>(rel.inverse());
+	}
+
+	@Override
+	public TO from(FROM from) {
+		return from_(from);
+	}
+
+	@Override
+	public FROM[] to(TO to) {
+		return tos_(to);
+	}
+
+	@Override
+	public long from(long fromId) {
+		return from_(fromId);
+	}
+
+	@Override
+	public long[] to(long toId) {
+		return tos_(toId);
+	}
+
+}
