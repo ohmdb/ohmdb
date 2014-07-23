46d292533e5da8cfa3ac8c73beccc16cbb409acd
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-07-23 23:28:26 +0200

    Initial commit: FriendsPerfTest.java

---
+++ ohmdb-test/src/test/java/com/ohmdb/perf/FriendsPerfTest.java
@@ -0,0 +1,103 @@
+package com.ohmdb.perf;
+
+/*
+ * #%L
+ * ohmdb-test
+ * %%
+ * Copyright (C) 2013 - 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.testng.annotations.Test;
+
+import com.ohmdb.abstracts.Any;
+import com.ohmdb.abstracts.RWRelation;
+import com.ohmdb.api.Ohm;
+import com.ohmdb.api.OhmDB;
+import com.ohmdb.impl.OhmDBImpl;
+import com.ohmdb.join.JoinBuilder;
+import com.ohmdb.join.JoinConfig;
+import com.ohmdb.join.JoinQuery;
+import com.ohmdb.join.LinkMatcher;
+import com.ohmdb.test.TestAny;
+import com.ohmdb.test.TestCommons;
+import com.ohmdb.util.Measure;
+import com.ohmdb.util.UTILS;
+
+public class FriendsPerfTest extends TestCommons {
+
+	private static final int USERS_N = 1 * 10000;
+
+	private static final int FRIENDS_N = 25;
+
+	@Test(timeOut = 60000)
+	public void shouldMatchNthConnections() throws Exception {
+		OhmDB db2 = Ohm.db();
+		LinkMatcher matcher = ((OhmDBImpl) db2).getLinkMatcher();
+
+		RWRelation rel = relation(db2, "friends");
+		rel.kind(true, true, true);
+
+		System.out.println("db ready");
+
+		Measure.start(USERS_N * FRIENDS_N);
+
+		int retries = 0;
+		for (int i = 0; i < USERS_N; i++) {
+			for (int j = 0; j < FRIENDS_N; j++) {
+				while (!rel.link(i, rnd(USERS_N))) {
+					retries++;
+				}
+			}
+		}
+
+		Measure.finish("link");
+
+		System.out.println("friends linked, retries: " + retries);
+
+		final int total = 1000;
+
+		Any<?> any1 = any(0, USERS_N);
+		Any<?> any2 = any(0, USERS_N);
+		Any<?> any3 = any(0, USERS_N);
+
+		JoinQuery query = JoinBuilder.make().join(0, rel, 1, rel, 2, rel, 3).join(3, rel, 4).build();
+
+		JoinConfig[] configs = new JoinConfig[total];
+
+		for (int i = 0; i < total; i++) {
+			int a = rnd(USERS_N);
+			int b = rnd(USERS_N);
+
+			configs[i] = jparam(query, UTILS.futureIds(nums(a)), UTILS.futureIds(any1), UTILS.futureIds(any2),
+					UTILS.futureIds(any3), UTILS.futureIds(nums(b)));
+		}
+
+		System.out.println("config ready!");
+
+		Measure.start(total);
+
+		for (int i = 0; i < total; i++) {
+			matcher.exists(configs[i]);
+		}
+
+		Measure.finish("Nth connection");
+	}
+
+	private TestAny any(int from, int to) {
+		return new TestAny(from, to);
+	}
+
+}
