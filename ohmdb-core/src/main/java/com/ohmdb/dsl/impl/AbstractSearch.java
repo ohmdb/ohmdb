6a71cce9e9e9303be25fd8ba808c6c20cac574b9
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-07-23 23:28:28 +0200

    Initial commit: AbstractSearch.java

---
+++ ohmdb-core/src/main/java/com/ohmdb/dsl/impl/AbstractSearch.java
@@ -0,0 +1,41 @@
+package com.ohmdb.dsl.impl;
+
+/*
+ * #%L
+ * ohmdb-core
+ * %%
+ * Copyright (C) 2013 - 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.ohmdb.api.BoundableSearch;
+import com.ohmdb.api.ParameterBinding;
+import com.ohmdb.api.SearchCriteria;
+import com.ohmdb.api.Table;
+
+public abstract class AbstractSearch<E> extends AbstractSearchActions<E> {
+
+	public AbstractSearch(Table<E> table) {
+		super(table);
+	}
+
+	@Override
+	public <T> BoundableSearch<E> bind(ParameterBinding<T> binding) {
+		return new BindSearchImpl<E>(table, this, binding);
+	}
+
+	protected abstract SearchCriteria query(BindSearchImpl<E> search);
+
+}
