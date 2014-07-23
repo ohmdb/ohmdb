2cae138bc79a4f6501e11ad403aa6c1d293f21ed
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-07-23 23:28:24 +0200

    Initial commit: Person2.java

---
+++ ohmdb-test/src/main/java/com/ohmdb/test/Person2.java
@@ -0,0 +1,85 @@
+package com.ohmdb.test;
+
+/*
+ * #%L
+ * ohmdb-test
+ * %%
+ * Copyright (C) 2013 - 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+public class Person2 {
+
+	private String name;
+	private int ageto;
+
+	public Person2(String name, int age) {
+		this.name = name;
+		this.ageto = age;
+	}
+
+	public Person2() {
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + ageto + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ageto;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person2 other = (Person2) obj;
+		if (ageto != other.ageto)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return ageto;
+	}
+
+	public void setAge(int age) {
+		this.ageto = age;
+	}
+
+}
