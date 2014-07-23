cedde1799c5b59907b9bd255be608a7c8af0a086
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-07-23 23:28:28 +0200

    Initial commit: StoreInfo.java

---
+++ ohmdb-core/src/main/java/com/ohmdb/filestore/StoreInfo.java
@@ -0,0 +1,48 @@
+package com.ohmdb.filestore;
+
+/*
+ * #%L
+ * ohmdb-core
+ * %%
+ * Copyright (C) 2013 - 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StoreInfo {
+
+	private final Map<String, PersistInfo> infos = new HashMap<String, PersistInfo>();
+
+	public synchronized PersistInfo getInfo(Object key) {
+		String str = String.valueOf(key);
+
+		PersistInfo info = infos.get(str);
+
+		if (info == null) {
+			info = new PersistInfo();
+			infos.put(str, info);
+		}
+
+		return info;
+	}
+
+	public synchronized Collection<PersistInfo> entries() {
+		return infos.values();
+	}
+
+}
