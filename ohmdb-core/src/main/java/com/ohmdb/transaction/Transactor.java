1d57ee557de2d74d4f752a9989e09ce4ce5b21e3
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-07-23 23:28:29 +0200

    Initial commit: Transactor.java

---
+++ ohmdb-core/src/main/java/com/ohmdb/transaction/Transactor.java
@@ -0,0 +1,35 @@
+package com.ohmdb.transaction;
+
+/*
+ * #%L
+ * ohmdb-core
+ * %%
+ * Copyright (C) 2013 - 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.ohmdb.api.Transaction;
+
+public interface Transactor {
+
+	void begin(Transaction transaction);
+
+	void commit(Transaction transaction);
+
+	void rollback(Transaction transaction);
+
+	Transaction getTransaction();
+
+}
