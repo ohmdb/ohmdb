195e94d5c5dc119a6f5b190d9e156b40027cf3a4
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2014-07-23 23:28:27 +0200

    Initial commit: Updaters.java

---
+++ ohmdb-core/src/main/java/com/ohmdb/abstracts/Updaters.java
@@ -0,0 +1,61 @@
+package com.ohmdb.abstracts;
+
+/*
+ * #%L
+ * ohmdb-core
+ * %%
+ * Copyright (C) 2013 - 2014 Nikolche Mihajlovski
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+public interface Updaters<E> {
+
+	// UpdateProp<E> set(byte property, byte value);
+	//
+	// UpdateProp<E> set(short property, short value);
+	//
+	// UpdateProp<E> set(int property, int value);
+	//
+	// UpdateProp<E> set(long property, long value);
+	//
+	// UpdateProp<E> set(float property, float value);
+	//
+	// UpdateProp<E> set(double property, double value);
+	//
+	// UpdateProp<E> set(boolean property, boolean value);
+	//
+	// UpdateProp<E> set(String property, String value);
+	//
+	// UpdateProp<E> set(Date property, Date value);
+	//
+	// UpdateProp<E> setProp(String propertyName, byte value);
+	//
+	// UpdateProp<E> setProp(String propertyName, short value);
+	//
+	// UpdateProp<E> setProp(String propertyName, int value);
+	//
+	// UpdateProp<E> setProp(String propertyName, long value);
+	//
+	// UpdateProp<E> setProp(String propertyName, float value);
+	//
+	// UpdateProp<E> setProp(String propertyName, double value);
+	//
+	// UpdateProp<E> setProp(String propertyName, boolean value);
+	//
+	// UpdateProp<E> setProp(String propertyName, String value);
+	//
+	// UpdateProp<E> setProp(String propertyName, Date value);
+
+}
